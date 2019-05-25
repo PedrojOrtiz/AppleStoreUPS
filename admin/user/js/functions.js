233af769b0a598aed1f71f28adb48cdafd99fbdb
Claudio <claum0395@gmail.com>
2019-05-25 11:31:48 -0500

    se actualiza de las tiendas los mapas con la ruta

--- admin/user/js/functions.js
+++ admin/user/js/functions.js
@@ -1,11 +1,36 @@
-function openWindow() {
+// function openWindow() {
 
-    let windowFloat = document.getElementById("floatWindow")
-    windowFloat.style.display = "flex"
+//     let windowFloat = document.getElementById("floatWindow")
+//     windowFloat.style.display = "flex"
 
-}
+// }
 
-function cluseWindow() {
-    let windowFloat = document.getElementById("floatWindow")
-    windowFloat.style.display = "none"
-}
+// function cluseWindow() {
+//     let windowFloat = document.getElementById("floatWindow")
+//     windowFloat.style.display = "none"
+// }
+
+// function loadMap(usuLat, usuLong, storeLat, storeLong) {
+
+//     var map = L.map('map').setView([usuLat, usuLong], 20);
+
+//     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//     }).addTo(map);
+
+//     // L.marker([usuLat, usuLong]).addTo(map)
+//     //     .bindPopup('Mi ubicacion.')
+//     //     .openPopup();
+
+//     // L.marker([storeLat, storeLong]).addTo(map)
+//     //     .bindPopup('Tienda.')
+//     //     .openPopup();
+
+//     L.Routing.control({
+//         waypoints: [
+//             L.latLng(usuLat, usuLong),
+//             L.latLng(storeLat, storeLong)
+//         ],
+//         routeWhileDragging: true,
+//     }).addTo(map);
+// }
--- public/js/map.js
+++ public/js/map.js
@@ -1,10 +1,28 @@
-function loadMap(lat, long, zoom, store, direccion) {
+function loadMap(usulat, usulong, lat, long, zoom, store, direccion) {
     var map = L.map('map').setView([lat, long], zoom);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker([lat, long]).addTo(map)
-        .bindPopup("<b>Apple Store " + store + "</b><br/>" + direccion).openPopup();
+    // L.marker([lat, long]).addTo(map)
+    //     .bindPopup("<b>Apple Store " + store + "</b><br/>" + direccion).openPopup();
+
+    // L.marker([usulat, usulong]).addTo(map)
+    //     .bindPopup("<b>Miubicacion</b><br />").openPopup();
+
+    L.Routing.control({
+        waypoints: [
+            L.latLng(usulat, usulong),
+            L.latLng(lat, long)
+        ],
+        routeWhileDragging: true,
+    }).addTo(map);
+
+    var testMarker = L.marker([lat, long]).bindPopup('<b>Apple Store ' + store + '</b><br/>' + direccion);
+    var testMarkerTwo = L.marker([usulat, usulong]).bindPopup('<b>Mi ubicacion</b>');
+    var markerGroup = L.featureGroup([testMarker, testMarkerTwo]).addTo(map);
+    markerGroup.eachLayer(function (layer) {
+        layer.openPopup();
+    });
 }
