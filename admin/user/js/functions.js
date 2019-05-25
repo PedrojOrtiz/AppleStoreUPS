314d77c213395c878bcc3d55ec999446e6ef0550
Claudio <claum0395@gmail.com>
2019-05-24 23:53:23 -0500

    se crea la ventana flotante para visualizar la ruta del mapa

---
+++ admin/user/js/functions.js
@@ -0,0 +1,11 @@
+function openWindow() {
+
+    let windowFloat = document.getElementById("floatWindow")
+    windowFloat.style.display = "flex"
+
+}
+
+function cluseWindow() {
+    let windowFloat = document.getElementById("floatWindow")
+    windowFloat.style.display = "none"
+}
