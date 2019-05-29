90e2f538b3119a0729b089fd068f78f4f101c5bd
YandryRo97 <yandry.yr99@gmail.com>
2019-05-28 21:18:48 -0500

    eliminar

---
+++ admin/admin/view/eliminar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Eliminar datos de persona</title>
+    </head>
+    <body>
+        <?php
+
+            //Incluir conexion a la BD
+            include '../../../config/configDB.php';
+            $codigo_admin = $_GET["codigo_admin"];
+            $codigo = $_POST["codigo"];
+            date_default_timezone_set("America/Guayaquil");
+            $fecha = date("Y-m-d H:i:s",time());
+            $sql = "UPDATE usuario SET usu_eliminado = 'S', usu_fecha_modificacion = '$fecha' WHERE usu_id = '$codigo'";
+
+            if ($conn->query($sql) == TRUE){
+                echo "<p>Se ha eliminado los datos correctamente</p>";
+            }else{
+                echo "<p>Error".$sql."<br>".mysqli_error($conn)."</p>";
+            }
+            echo "<a href='clients.php?codigo_admin=".$codigo_admin."'>Regresar</a>";
+
+            $conn->close();
+        ?>
+    </body>
+</html>
