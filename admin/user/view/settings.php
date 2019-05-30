17000a130eab0b156ddde336651753f46cadc530
JonnathanOchoa <UPSJhona@users.noreply.github.com>
2019-05-30 15:26:35 -0500

    Correcciones

--- admin/user/view/settings.php
+++ admin/user/view/settings.php
@@ -14,8 +14,7 @@ if (isset($_SESSION['isLogin'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-        integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet">
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../../../public/css/globalStyle.css">
@@ -42,7 +41,9 @@ if (isset($_SESSION['isLogin'])) {
                 <div class="updatePass">
                     <h2>Cambiar contraseña</h2>
                     <div class="formData updatePass">
-                        <form action="">
+                        <form action="../controller/updatePass.php">
+
+                            <input type="hidden" name="cod" value="<?php echo ($_GET["usu_id"]); ?>">
                             <label for="oldpass">Contraseña antigua</label>
                             <input type="password" name="oldpass" id="oldpass">
                             <label for="newpass">Contraseña nueva</label>
