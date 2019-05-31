b11ffcadfea0ca84aa27ef89847fe1a9fb0a90e5
YandryRo97 <yandry.yr99@gmail.com>
2019-05-31 17:38:31 -0500

    css de cliente

--- admin/admin/controller/modificar.php
+++ admin/admin/controller/modificar.php
@@ -1,14 +1,98 @@
+<?php 
+
+    include '../../../config/configDB.php';
+
+    session_start();
+
+    if (isset($_SESSION['codigo']))
+        $id=$_SESSION['codigo'];
+
+    if($_SESSION["rol"] != "admin")
+        header("Location: ../controller/logout.php");
+
+    $sqlUsuario = "SELECT * FROM usuario user, imagen img WHERE user.usu_id = $id AND img.USUARIO_usu_id = $id";
+
+    $resultUsuario = $conn->query($sqlUsuario);
+    $rowUsuario = mysqli_fetch_assoc($resultUsuario);
+
+    $nombres = $rowUsuario['usu_nombres'];
+    $apellidos = $rowUsuario['usu_apellidos'];
+    $img = $rowUsuario['img_nombre'];
+
+    $sucId = $rowUsuario['SUCURSAL_suc_id'];
+
+    $sqlSucursal = "SELECT * FROM sucursal suc WHERE suc.suc_id = $sucId";
+
+    $resultSucursal = $conn->query($sqlSucursal);
+    $rowSucursal = mysqli_fetch_assoc($resultSucursal);
+
+    $sucNombre = $rowSucursal['suc_nombre'];
+    $sucTelefono = $rowSucursal['suc_telefono'];
+    $sucCelular = $rowSucursal['suc_celular'];
+    $sucUrl = $rowSucursal['suc_url'];
+    $sucEliminado = $rowSucursal['suc_eliminado'];
+
+?>
+
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Modificar datos de persona</title>
-    </head>
-    <body>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+        integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/globalStyle.css">
+    <link rel="stylesheet" href="../css/style2.css">
+    <title>Perfil</title>
+</head>
+
+<body>
+    <header>
+        <div class="content">        
+            <div class="sessionItems">
+                    <div class="header">
+                        <ul class="nav">
+                            <li> <a><?php echo strtoupper($nombres) ?> <?php echo strtoupper($apellidos) ?></a>
+                                <ul>
+                                    <li><a href="modify.php">Ajustes</a></li>
+                                    <li><a href="../controller/logout.php">Cerrar Sesion</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                <div class="imgUser">
+                    <img src="../../../img/user/<?php echo $id; ?>/<?php echo ($img); ?>" alt="">
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <div class="container">
+        <header>
+            <div class="perfil">
+                <li><a>APPLE STORE EC</a></li>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="index.php"><i class="fas fa-home"></i> Inicio</a></li>
+                    <li><a href="orders.php"><i class="fas fa-chart-bar"></i> Ordenes</a></li>
+                    <li><a href="inbox.php"><i class="far fa-envelope"></i> Mensajes</a></li>
+                    <li><a href="products.php"><i class="fa fa-barcode"></i> Productos</a></li>
+                    <li><a href="clients.php"><i class="fas fa-users"></i> Clientes</a></li>
+                    <li><a href="settings.php"><i class="fas fa-cog"></i> Configuracion</a></li>
+                </ul>
+            </nav>
+        </header>
+        <section>
+            <div class="cardContent">
         <?php
             //Incluir conexion a la base de datos
-            include '../../../config/configDB.php';
-            $codigo_admin = $_GET["codigo_admin"];
+            
+            
 
             $codigo = $_POST["codigo"];
             $nombres = isset($_POST["nombres"]) ? mb_strtoupper(trim($_POST["nombres"]), 'UTF-8') : null;
@@ -25,9 +109,11 @@
             }else{
                 echo "Error: ".$sql."<br>".mysqli_error($conn)."<br>";
             }
-            echo "<a href='../view/clients.php?codigo_admin=".$codigo_admin."'>Regresar</a>";
+            echo "<a href='../view/clients.php?codigo_admin=".$codigo."'>Regresar</a>";
 
             $conn->close();
         ?>
+        </div>
+        </section>
     </body>
 </html>
