cf4b801038b8eb5cf916f2fa760026081b69dc46
HenryGuaman <34074520+HenryGuaman@users.noreply.github.com>
2019-06-02 14:53:45 -0500

    Actualizacion y verificacion de datos en Sucursal cuenca

--- public/view/storecuenca.php
+++ public/view/storecuenca.php
@@ -56,9 +56,9 @@ if (isset($_SESSION['isLogin'])) {
                     <h3>Horarios de atencion</h3>
                     <p>Lunes a Viernes de 8:00 a 16:00</p>
                     <p><i class="fas fa-phone"></i> 0989420495</p>
-                    <p><i class="fas fa-tty"></i> (07)2255887</p>
+                    <p><i class="fas fa-tty"></i> (07)32564</p>
                     <p><i class="fas fa-at"></i> <a
-                            href="mailto:aplestoreguayaquil@aple.com">applestoreguayaquil@apple.com</a></p>
+                            href="mailto:aplestoreguayaquil@aple.com">applestorecuenca@apple.com.ec</a></p>
                     <p><i class="fas fa-map-marker-alt"></i> Av.Juan Tamariz y 3 de Octubre</p>
                 </div>
             </div>
