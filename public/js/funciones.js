4d8906602b2110f46152e498c1f393b236d74879
JonnathanOchoa <UPSJhona@users.noreply.github.com>
2019-06-03 16:11:09 -0500

    Correciones

--- public/js/funciones.js
+++ public/js/funciones.js
@@ -1,96 +1,3 @@
-function openWindow() {
-    let windowFloat = document.getElementById("floatWindow")
-    windowFloat.style.display = "flex"
-}
-
-function cluseWindow() {
-    let windowFloat = document.getElementById("floatWindow")
-    windowFloat.style.display = "none"
-}
-//PENDIENTE
-function elemento(e) {
-    let elements = document.getElementById('valoration')
-
-    for (let i = 0; i < elements.length; i++) {
-        console.log(elements[i].srcElement)
-
-    }
-
-    if (e.srcElement)
-        tag = e.srcElement.tagName;
-    else if (e.target)
-        tag = e.target.tagName;
-    //console.log("El elemento selecionado ha sido " + tag);
-}
-
-function stock(elemnt) {
-    //console.log(elemnt.selectedIndex)
-    //console.log(text)
-    if (window.XMLHttpRequest) {
-        xmlhttp = new XMLHttpRequest()
-    } else {
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP")
-    }
-    xmlhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("stok").innerHTML = this.responseText
-        }
-    };
-    xmlhttp.open("GET", "../controller/stock.php?idx=" + (elemnt.selectedIndex + 1), true)
-    xmlhttp.send()
-}
-
-function searchBtn(url) {
-    //console.log('hola')
-    let txtSearch = document.getElementById('search').value
-    txtSearch = txtSearch.toUpperCase()
-    window.location.href = url + '?searchName=' + txtSearch
-}
-
-function searchBox(elemnt) {
-    let text = elemnt.value.trim()
-    text = text.toUpperCase()
-    //console.log(text)
-    if (window.XMLHttpRequest) {
-        xmlhttp = new XMLHttpRequest()
-    } else {
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP")
-    }
-    xmlhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("contentCards").innerHTML = this.responseText
-        }
-    };
-    xmlhttp.open("GET", "../controller/search.php?searchName=" + text, true)
-    xmlhttp.send()
-}
-
-function cartAdd(cod) {
-    //console.log(cod)
-    //console.log(precioTotal)
-    let storeId = (document.getElementById('selectStore').selectedIndex + 1)
-    //console.log("IDX STORE" + storeId)
-
-    if (window.XMLHttpRequest) {
-        xmlhttp = new XMLHttpRequest()
-    } else {
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP")
-    }
-    xmlhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            carNot()
-            if (document.getElementById("cartAdd") === null) {
-                document.body.innerHTML += this.responseText
-            } else {
-                openWindowCart()
-            }
-            //document.getElementById('body').innerHTML = this.responseText
-        }
-    };
-    xmlhttp.open("GET", "../controller/cartAdd.php?codProd=" + cod + "&storeID=" + storeId, true)
-    xmlhttp.send()
-}
-
 function openWindowCart() {
     let windowFloat = document.getElementById("cartAdd")
     windowFloat.style.display = "flex"
@@ -190,5 +97,97 @@ function carNot(url) {
         }
     };
     xmlhttp.open("GET", url, true)
+    xmlhttp.send()
+}
+function openWindow() {
+    let windowFloat = document.getElementById("floatWindow")
+    windowFloat.style.display = "flex"
+}
+
+function cluseWindow() {
+    let windowFloat = document.getElementById("floatWindow")
+    windowFloat.style.display = "none"
+}
+//PENDIENTE
+function elemento(e) {
+    let elements = document.getElementById('valoration')
+
+    for (let i = 0; i < elements.length; i++) {
+        console.log(elements[i].srcElement)
+
+    }
+
+    if (e.srcElement)
+        tag = e.srcElement.tagName;
+    else if (e.target)
+        tag = e.target.tagName;
+    //console.log("El elemento selecionado ha sido " + tag);
+}
+
+function stock(elemnt) {
+    //console.log(elemnt.selectedIndex)
+    //console.log(text)
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest()
+    } else {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP")
+    }
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("stok").innerHTML = this.responseText
+        }
+    };
+    xmlhttp.open("GET", "../controller/stock.php?idx=" + (elemnt.selectedIndex + 1), true)
+    xmlhttp.send()
+}
+
+function searchBtn(url) {
+    //console.log('hola')
+    let txtSearch = document.getElementById('search').value
+    txtSearch = txtSearch.toUpperCase()
+    window.location.href = url + '?searchName=' + txtSearch
+}
+
+function searchBox(elemnt) {
+    let text = elemnt.value.trim()
+    text = text.toUpperCase()
+    //console.log(text)
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest()
+    } else {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP")
+    }
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("contentCards").innerHTML = this.responseText
+        }
+    };
+    xmlhttp.open("GET", "../controller/search.php?searchName=" + text, true)
+    xmlhttp.send()
+}
+
+function cartAdd(cod) {
+    //console.log(cod)
+    //console.log(precioTotal)
+    let storeId = (document.getElementById('selectStore').selectedIndex + 1)
+    //console.log("IDX STORE" + storeId)
+
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest()
+    } else {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP")
+    }
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            carNot()
+            if (document.getElementById("cartAdd") === null) {
+                document.body.innerHTML += this.responseText
+            } else {
+                openWindowCart()
+            }
+            //document.getElementById('body').innerHTML = this.responseText
+        }
+    };
+    xmlhttp.open("GET", "../controller/cartAdd.php?codProd=" + cod + "&storeID=" + storeId, true)
     xmlhttp.send()
 }
